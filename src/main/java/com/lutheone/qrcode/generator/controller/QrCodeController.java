9c2d0be37200fc7ebc0a6c0d4da83f117a8f2863
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-08-25 17:28:38 -0300

    feat: add QRCodeResponse DTO returning QR code URL

--- src/main/java/com/lutheone/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/lutheone/qrcode/generator/controller/QrCodeController.java
@@ -1,6 +1,7 @@
 package com.lutheone.qrcode.generator.controller;
 
 import com.lutheone.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.lutheone.qrcode.generator.dto.QrcodeGenerateResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class QrCodeController {
 
     @PostMapping
-    public ResponseEntity<> generateQrCode(@RequestBody QrCodeGenerateRequest request) {
-
-
-        // QR code generation logic will go here
+    public ResponseEntity<QrcodeGenerateResponse> generateQrCode
+            (@RequestBody QrCodeGenerateRequest request) {
+    return null;
+    // QR code generation logic will go here
     }
 }
---
+++ src/main/java/com/lutheone/qrcode/generator/dto/QrcodeGenerateResponse.java
@@ -0,0 +1,4 @@
+package com.lutheone.qrcode.generator.dto;
+
+public record QrcodeGenerateResponse(String qrCodeUrl) {
+}
